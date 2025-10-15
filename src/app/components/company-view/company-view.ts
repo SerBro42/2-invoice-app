a96b0802ad64c470012f12f59a8c3ae571b3b822
Sergey Brovko <serbro7@gmail.com>
2025-10-15 18:00:24 +0200

    Sub-components: company view and customer view implemented

--- src/app/components/company-view/company-view.ts
+++ src/app/components/company-view/company-view.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Company } from '../../models/company';
 
 @Component({
-  selector: 'app-company-view',
+  selector: 'company-view',
   imports: [],
   templateUrl: './company-view.html'
 })
 export class CompanyViewComponent {
+
+  //These data come from the parent component, hence we use @Input
+  @Input() company: Company = new Company();
 
 }
--- src/app/components/customer-view/customer-view.ts
+++ src/app/components/customer-view/customer-view.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Client } from '../../models/client';
 
 @Component({
-  selector: 'app-customer-view',
+  selector: 'customer-view',
   imports: [],
   templateUrl: './customer-view.html'
 })
 export class CustomerViewComponent {
+
+  //These data come from the parent component, hence we use @Input
+  @Input() client: Client = new Client();
 
 }
